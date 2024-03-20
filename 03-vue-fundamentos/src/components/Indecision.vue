723d7eeb56b23c9668dc5f82cbe61e5ca51872c2
Gero Nñz <gerons69@gmail.com>
2024-03-19 19:18:14 -0600

    ..

--- 03-vue-fundamentos/src/components/Indecision.vue
+++ 03-vue-fundamentos/src/components/Indecision.vue
@@ -4,12 +4,12 @@
         <div class="bg-dark"></div>
 
         <div class="indecision-container">
-            <input type="text" placeholder="hazme una pregunta">
+            <input v-model="question" type="text" placeholder="hazme una pregunta">
             <p>Recuerda terminar con un signo de interrogación (?)</p>
 
             <div>
-                <h2>¿Seré millonario?</h2>
-                <h1>si, no, pensando...</h1>
+                <h2>{{ question }}</h2>
+                <h1>Si, No, pensando...</h1>
             </div>
         </div>
     </div>
@@ -22,12 +22,60 @@ export default {
     },
     data() {
         return {
-            variable: ''
+            question: null
+        }
+    },
+    watch: {
+        question(value) {
+            if (!value.includes('?')) return
         }
     }
 }
 </script>
 
 <style scoped>
+
+    img, .bg-dark {
+        height: 100vh;
+        left: 0px;
+        max-height: 100%;
+        max-width: 100%;
+        position: fixed;
+        top: 0px;
+        width: 100vw;
+    }
+
+    .bg-dark {
+        background-color: rgba(0, 0, 0, 0.4);
+    }
+
+    .indecision-container {
+        position: relative;
+        z-index: 99;
+    }
+    
+    input {
+        width: 250px;
+        padding: 10px 15px;
+        border-radius: 5px;
+        border: none;
+    }
+    input:focus {
+        outline: none;
+    }
+
+    p {
+        color: white;
+        font-size: 20px;
+        margin-top: 0px;
+    }
+
+    h1, h2 {
+        color: white;
+    }
+    
+    h2 {
+        margin-top: 150px;
+    }
 
 </style>
