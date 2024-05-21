8d01e99d22325838c495ceda431793685d5babec
Gero Nñz <gerons69@gmail.com>
2024-05-20 19:18:50 -0600

    ..

---
+++ 03-indecision-app/src/components/MyCounter.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <h3>Counter: {{ contador }}</h3>
+        <h3>Square: {{ squareCounter  }}</h3>
+
+        <div>
+            <button @click="contador++">+1</button>
+            <button @click="contador--">-1</button>
+        </div>
+    </div>
+</template>
+
+<script lang="ts"  setup>
+import { computed, ref } from 'vue'
+
+const contador = ref(2)
+
+const squareCounter = computed(() => {
+    return contador.value * contador.value
+})
+
+</script>
